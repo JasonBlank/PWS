82011bf4ff5292ba4b9a2bcc612f1cfec6b460d3
Jason Blank <jason.blank@outlook.com>
2017-01-09 13:28:12 +0100

    Wissel van 0 en 1 naar true en false

--- Code/pws/algoritme/Rij.java
+++ Code/pws/algoritme/Rij.java
@@ -21,69 +21,55 @@ public class Rij {
 	void checknPlace(int wt, Vliegtuig vt){								//Hoofd controle functie; wt is Wanted Time
 
 
-		switch (checkBefore(wt, vt)){									//0 is problematisch; 1 is ruimte
-			case 0:
-				switch (checkAfter(wt,vt)){
-					case 0:
-						//nu moeten we gaan schuiven. Aan allebei de kanten zit een vliegtuig.
-						break;
-					case 1: //Rechts is ruimte, links niet.
-						if(checkBefore(wt-SepTime.getSepTime(abraham.getKlasse(),vt.getKlasse()),abraham) == 1){			//hier kijken of er ruimte is is voor abraham om naar links te gaan
-							abraham.assignTime(abraham.getAt() - ((abraham.getAt()+sep)-wt));
-							//Extra kosten van deze stap = (abraham.getAt()+sep-wt)*kosten van te vroeg;
-							vtl[wt-bt] = vt;
-						}
-					else{
-						if(checkAfter(abraham.getAt()+sep,vt) == 1){
-							vtl[abraham.getAt()+sep] = vt;
-							vt.assignTime(abraham.getAt()+sep);
-							vt.setV_current(vt.getAfstand()/(vt.getAt()-ct));
-							//extra kosten deze stap = ((wt-abraham.getAt()+sep)*Kosten van te laat;
-					} 	else{
-							checknPlace(abraham.getAt()+sep,vt);
-							//chenknPlace(abraham.getAt()+sep -1, vt);
-							//we willen dus dat we hier sws naar case 0.0 gaan. Zie voor uitleg 1.1
-							//dit kunnen we doen zoals bij 1.1 uitgelegd. Maar ook mis door ze ze meteen door te sturen naar 0.0
-							//maar ik denk dat het makkelijkst is om manier te doen zoals bij 1.1 omdat het anders wel erg moeilijk wordt om het te programmeren in 0.0
-						}
+		if (checkBefore(wt, vt)) {                                    //false is problematisch; true is ruimte
+			if (checkAfter(wt, vt)) {
+				//nu moeten we gaan schuiven. Aan allebei de kanten zit een vliegtuig.
+			} else {//Rechts is ruimte, links niet.
+				if (checkBefore(wt - SepTime.getSepTime(abraham.getKlasse(), vt.getKlasse()), abraham) == true) {            //hier kijken of er ruimte is is voor abraham om naar links te gaan
+					abraham.assignTime(abraham.getAt() - ((abraham.getAt() + sep) - wt));
+					//Extra kosten van deze stap = (abraham.getAt()+sep-wt)*kosten van te vroeg;
+					vtl[wt - bt] = vt;
+				} else {
+					if (checkAfter(abraham.getAt() + sep, vt) == true) {
+						vtl[abraham.getAt() + sep] = vt;
+						vt.assignTime(abraham.getAt() + sep);
+						vt.setV_current(vt.getAfstand() / (vt.getAt() - ct));
+						//extra kosten deze stap = ((wt-abraham.getAt()+sep)*Kosten van te laat;
+					} else {
+						checknPlace(abraham.getAt() + sep, vt);
+						//chenknPlace(abraham.getAt()+sep -1, vt);
+						//we willen dus dat we hier sws naar case false.false gaan. Zie voor uitleg true.true
+						//dit kunnen we doen zoals bij true.true uitgelegd. Maar ook mis door ze ze meteen door te sturen naar false.false
+						//maar ik denk dat het makkelijkst is om manier te doen zoals bij true.true omdat het anders wel erg moeilijk wordt om het te programmeren in false.false
 					}
-						//Rechts is ruimte maar kijk eerst of V1 naar links kan; is goedkoper.
-						break;
 				}
-				break;
-
-			case 1:
-				switch (checkAfter(wt,vt)){
-					case 0:
-						//Links is ruimte dus op naar links met Vx.
-						//Zo verder kijken
-						if(checkBefore(abraham.getAt()-sep,vt) == 1){
-								vtl[abraham.getAt()-sep] = vt;
-								vt.assignTime(abraham.getAt()-sep);
-								vt.setV_current(vt.getAfstand()/(vt.getAt()-ct));
-								//extra kosten deze stap = ((abraham.getAt()-sep)-wt)*Kosten van te vroeg;
-						} else{
-							checknPlace(abraham.getAt()-sep,vt);
-							//chenknPlace(abraham.getAt()-sep +1, vt);
-							//hier gaat het fout nu. Want als je hem nu op nieuw laat lopen komt hij als het goed is uit bij 0.1 omdat er precies genoeg ruimte zou zijn om geen last te hebben van die van rechts.
-							//In de 1.0 als hij niet naar links kan schuiven gaat hij weer terug naar wanneer hij precies geen lans meer heeft van links. En op die manier blijft hij dan bounchen tussen die twee.
-							//We moeten er dus voor zorgen dat we de wt niet veranderen in precies sep er vanaf maar iets minder dat hij bij 0.0 komt zodat we het kunnen oplossen.
-						}
-
-
-						break;
-					case 1:
-						//Jee alle ruimte die er is. Nu lekker plaatsen op de optimale tijd.
-						//done
-						int diff = (int)wt;
-						vtl[diff] = vt;
-						vt.assignTime(diff);
-						vt.setV_current(vt.getAfstand()/(vt.getAt()-ct));
-						System.out.println("Placement successful. Assigned time: "+vt.getAt());
-						//extra kosten = 0
-						break;
+			}
+		} else{
+			if(checkAfter(wt,vt)) {									//Niet schrikken, ik heb 0 en 1 vervangen voor true en false, en de switch toch maar een if gemaakt
+				//Links is ruimte dus op naar links met Vx.
+				//Zo verder kijken
+				if (checkBefore(abraham.getAt() - sep, vt) == true) {
+					vtl[abraham.getAt() - sep] = vt;
+					vt.assignTime(abraham.getAt() - sep);
+					vt.setV_current(vt.getAfstand() / (vt.getAt() - ct));
+					//extra kosten deze stap = ((abraham.getAt()-sep)-wt)*Kosten van te vroeg;
+				} else {
+					checknPlace(abraham.getAt() - sep, vt);
+					//chenknPlace(abraham.getAt()-sep +1, vt);
+					//hier gaat het fout nu. Want als je hem nu op nieuw laat lopen komt hij als het goed is uit bij false.true omdat er precies genoeg ruimte zou zijn om geen last te hebben van die van rechts.
+					//In de true.false als hij niet naar links kan schuiven gaat hij weer terug naar wanneer hij precies geen lans meer heeft van links. En op die manier blijft hij dan bounchen tussen die twee.
+					//We moeten er dus voor zorgen dat we de wt niet veranderen in precies sep er vanaf maar iets minder dat hij bij false.false komt zodat we het kunnen oplossen.
 				}
-				break;
+			} else{
+					//Jee alle ruimte die er is. Nu lekker plaatsen op de optimale tijd.
+					//done
+					int diff = (int)wt;
+					vtl[diff] = vt;
+					vt.assignTime(diff);
+					vt.setV_current(vt.getAfstand()/(vt.getAt()-ct));
+					System.out.println("Placement successful. Assigned time: "+vt.getAt());
+					//extra kosten = 0
+			}
 		}
 
 
@@ -91,7 +77,7 @@ public class Rij {
 
 
 
-	private int checkBefore(long wt, Vliegtuig vt){		//Is er al een vliegtuig gepland op het gewenste tijdstip?
+	private boolean checkBefore(long wt, Vliegtuig vt){		//Is er al een vliegtuig gepland op het gewenste tijdstip?
 
 		for(int i = 0; i <= maxsep; i++) {
 			if(wt-i >= 0 && wt < 2100) {
@@ -101,19 +87,19 @@ public class Rij {
 
 					for(int j = 0; j<= sep; j++) {
 						if (vtl[(int) wt - bt - j] != null) {
-							return 0;
+							return false;
 						}
 					}
-					return 1;
+					return true;
 				}
 			}
 		}
 
 
-		return 1;
+		return true;
 	}
 
-	private int checkAfter(long wt, Vliegtuig vt){							//Nog verbeteren
+	private boolean checkAfter(long wt, Vliegtuig vt){							//Nog verbeteren
 		for(int i = 0; i <= maxsep; i++) {
 			if(wt-bt+i < 2100) {
 				if (vtl[(int) wt - bt + i] != null) {
@@ -121,16 +107,16 @@ public class Rij {
 					sep = SepTime.getSepTime(abraham.getKlasse(), vt.getKlasse());
 					for (int j = 0; j <= sep; j++) {
 						if (vtl[(int) wt - bt + j] != null) {
-							return 0;
+							return false;
 						}
 					}
-					return 1;
+					return true;
 				}
 			}
 		}
 
 
-		return 1;
+		return true;
 	}
 
 
