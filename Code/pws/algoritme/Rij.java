8c5d22cd4b8aec8282dbf204a36b2e78d96f361c
Jason Blank <jason.blank@outlook.com>
2017-01-13 14:27:09 +0100

    Fucking merge

--- Code/pws/algoritme/Rij.java
+++ Code/pws/algoritme/Rij.java
@@ -232,11 +232,6 @@ public class Rij {
 							return true;
 						}
 					}
-<<<<<<< HEAD
-					System.out.println("CheckAfter() heeft geen vliegtuigen gevonden na "+vt + " gevonden");
-					return false;
-=======
->>>>>>> origin/master
 				}
 			}
 		}
